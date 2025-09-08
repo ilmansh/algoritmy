55ee4b9b3a201506d4a84a202537dff3a4d5e82f
ilmansh <157303322+ilmansh@users.noreply.github.com>
2025-09-08 11:54:22 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+int main() {
+  // Стек
+  stack<string> st;
+  st.push("А"); // добавляем(в конец)
+  st.push("Б"):
+  cout << "Верхушка:" << st.top() << endl;
+  st.pop(); // удаляем(верх)
+  cout << "Верхушка:" << st.top() << endl;
+
+  // Массив
+  vector<int> array = {5, 3, 6, 1};
+  cout << "Элемент с индексом 2: " << array[2] << endl;
+
+  array.push_back(11);
+  cout << array << endl;
+  array.erase(array.begin() + 1); // удаляем второй элемент
+  cout << array << endl;
+}
