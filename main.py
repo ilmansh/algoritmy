0d86a9cecd76ec94e77f69f7b310c1ef24a37d22
ilmansh <157303322+ilmansh@users.noreply.github.com>
2025-09-08 11:42:49 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+# Стек
+stack = []
+stack.append("А") # добавление элемента
+stack.append("Б")
+print(stack)
+up = stack.pop() # удаление элемента
+print(up)
+print(stack)
+
+#  Массив
+array = [5, 3, 6, 1]
+print(array)
+# Из массива можно достать определенный элемент по индексу
+print(array[2])
+
+array.append(8) # можно добавить элемент
+print(array)
+arrey.remove(3) # можно удалить элемент
+print(array)
